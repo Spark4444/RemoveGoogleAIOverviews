8d5b4477d83fe04df276ee1bf099f1251f8f1b63
Spark4444 <102835794+Spark4444@users.noreply.github.com>
2025-07-31 19:43:10 +0300

    Version 1.7.0 small improvments in code

--- RemoveGoogleAIOverviews/js/website.js
+++ RemoveGoogleAIOverviews/js/website.js
@@ -1,3 +1,4 @@
+// Functions
 // Function to save data to Chrome storage
 function saveToChromeStorage(key, value) {
     chrome.storage.sync.set({[key]: value});
@@ -20,49 +21,57 @@ function checkIfAValueIsSet(value, defaultValue){
     }
 }
 
-// Function to remove the AI overview element
-function removeAIOverview(remove = true) {
-    if (remove){
-        aiOverview.remove();
-
-        setTimeout(() => {
-            let aiOverview = document.querySelector("#eKIzJc");
-            if (aiOverview) {
-                aiOverview.remove();
-            }
-        }, 1000);
-    }
+function selectAiOverviewElement() {
+    return document.querySelector("#eKIzJc");
 }
 
 // Variables
-let aiOverview = document.querySelector("#eKIzJc");
 let removeGoogleAIOverviews = true;
+
+// Function to remove the AI overview element
+function removeAIOverview(remove = true) {
+    // Check the argument remove and check if the element exists
+    let aiOverview = selectAiOverviewElement();
+    if (remove && aiOverview) {
+        aiOverview.remove();
+
+        const interval = setInterval(() => {
+            aiOverview = selectAiOverviewElement();
+            if (aiOverview) {
+                aiOverview.remove();
+            }
+        }, 100);
+
+        // Stop the interval after 5 seconds to prevent memory leaks
+        setTimeout(() => {
+            clearInterval(interval);
+        }, 5000);
+    }
+}
 
 // Get the value from Chrome storage
 getFromChromeStorage("removeGoogleAIOverviews", (value) => {
-    removeGoogleAIOverviews = checkIfAValueIsSet(value, true);
+    removeGoogleAIOverviews = checkIfAValueIsSet(value, false);
+    saveToChromeStorage("removeGoogleAIOverviews", removeGoogleAIOverviews);
 
     // Remove the AI overview if the setting is enabled
     // The "removeGoogleAIOverviews" varaible is reversed to match the checkbox state
-    if (aiOverview && !removeGoogleAIOverviews) {
-        removeAIOverview();
-    }
+    removeAIOverview(!removeGoogleAIOverviews);
 });
 
-// Update the page if the setting changes when the tab is focused (e.g. when the user returns to the tab)
-window.addEventListener("focus", () => {
-    getFromChromeStorage("removeGoogleAIOverviews", (value) => {
-        let currentRemoveAIOverviews = checkIfAValueIsSet(value, true);
-        
-        if (currentRemoveAIOverviews !== removeGoogleAIOverviews) {
+// Listen for changes in Chrome storage to update the page
+// If value changes, reload the page
+
+// Why use this listener over window "focus" event?
+// 1. This event will work even if the tab is running in the background.
+// 2. This event only triggers when the value changes, not when the tab is focused so more efficient.
+// 3. Focus is not reliable when the user is in the current tab, you would have to send a message from the pop up script to the content script to reload the page, which is more complex and less efficient.
+// 4. This event listener takes care of both scenarios
+chrome.storage.onChanged.addListener((changes, area) => {
+    if (area === "sync" && changes.removeGoogleAIOverviews) {
+        const newValue = changes.removeGoogleAIOverviews.newValue;
+        if (newValue !== removeGoogleAIOverviews) {
             window.location.reload();
         }
-    });
-});
-
-// Listen for messages from the popup to update the page if the setting changes
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "toggleChange") {
-        window.location.reload();
     }
 });
